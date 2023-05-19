985b7caedbb6b87ad2174a563bc6e72a92da9f18
SHEKHAR2002 <shekharingale14@gmail.com>
2023-05-20 03:30:49 +0530

    search page half frontend added

--- lib/screens/searchpage.dart
+++ lib/screens/searchpage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:musicapp/Theme/color.dart';
 
@@ -12,11 +13,68 @@ class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Text(
-        "Search page",
-        style: TextStyle(color: white),
-      )),
-    );
+        body: SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Row(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: transparntgrey,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Icon(
+                  CupertinoIcons.back,
+                  size: 30,
+                  color: white,
+                ),
+              ),
+            ),
+            const Spacer(),
+            Container(
+              decoration: BoxDecoration(
+                color: transparntgrey,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Icon(
+                  CupertinoIcons.heart,
+                  size: 30,
+                  color: white,
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        const Text(
+          "Recent favourites",
+          style: TextStyle(
+              color: Colors.white, fontSize: 28, fontWeight: FontWeight.w500),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        GridView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: 6,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, mainAxisSpacing: 5, crossAxisSpacing: 5),
+            itemBuilder: (context, index) => Container(
+                  height: 140,
+                  width: 140,
+                  decoration: BoxDecoration(
+                    color: transparntgrey,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ))
+      ]),
+    ));
   }
 }
