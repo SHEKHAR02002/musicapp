ff3d7439ac67ddefa8c2a17931e062afd0dc6649
SHEKHAR2002 <shekharingale14@gmail.com>
2023-05-20 15:55:50 +0530

    second page front end completed

--- lib/screens/searchpage.dart
+++ lib/screens/searchpage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:musicapp/Theme/color.dart';
+import 'package:musicapp/widget/recentfavcard.dart';
 
 class Search extends StatefulWidget {
   const Search({super.key});
@@ -10,6 +11,20 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
+  // List images = [
+  //   'assets/images/one.png',
+  //   'assets/images/two.jpg',
+  //   'assets/images/three.jpg'
+  // ];
+  // List title = [
+  //   'Bright Hits',
+  //   'Millions',
+  //   'Family',
+  //   'Millions',
+  //   'Hindi',
+  //   'Marathi'
+  // ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -65,16 +80,26 @@ class _SearchState extends State<Search> {
             physics: const NeverScrollableScrollPhysics(),
             itemCount: 6,
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, mainAxisSpacing: 5, crossAxisSpacing: 5),
-            itemBuilder: (context, index) => Container(
-                  height: 140,
-                  width: 140,
-                  decoration: BoxDecoration(
-                    color: transparntgrey,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
+                crossAxisCount: 2, mainAxisSpacing: 20, crossAxisSpacing: 5),
+            itemBuilder: (context, index) => RecentFavCard(
+                  choice: choices[index],
                 ))
       ]),
     ));
   }
 }
+
+class Choice {
+  const Choice({required this.title, required this.icon});
+  final String title;
+  final String icon;
+}
+
+const List<Choice> choices = <Choice>[
+  Choice(title: 'Bright Hitd', icon: 'assets/images/one.png'),
+  Choice(title: 'Millions', icon: 'assets/images/two.jpg'),
+  Choice(title: 'Family', icon: 'assets/images/three.jpg'),
+  Choice(title: 'Millions', icon: 'assets/images/one.png'),
+  Choice(title: 'Hindi', icon: 'assets/images/two.jpg'),
+  Choice(title: 'Marathi', icon: 'assets/images/three.jpg'),
+];
---
+++ lib/widget/recentfavcard.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:musicapp/Theme/color.dart';
+import 'package:musicapp/screens/searchpage.dart';
+
+class RecentFavCard extends StatefulWidget {
+  final Choice choice;
+  const RecentFavCard({super.key, e, required this.choice});
+
+  @override
+  State<RecentFavCard> createState() => _RecentFavCardState();
+}
+
+class _RecentFavCardState extends State<RecentFavCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 250,
+      color: black,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 140,
+              width: 140,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(widget.choice.icon),
+                  fit: BoxFit.fill,
+                ),
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            Text(
+              widget.choice.title,
+              style: TextStyle(
+                  fontSize: 16, fontWeight: FontWeight.w500, color: white),
+            ),
+            Text(
+              widget.choice.title,
+              style: TextStyle(fontSize: 12, color: grey),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
